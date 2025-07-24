bd1281b2b1848de4b9c88364a1842cb085b58fd8
x <xaviertears.dev@gmail.com>
2025-07-24 17:20:30 +0800

    Update PokedexController.java

--- controller/PokedexController.java
+++ controller/PokedexController.java
@@ -4,6 +4,20 @@ import java.util.*;
 public class PokedexController {
     private List<Pokemon> pokemons = new ArrayList<>();
 
+    public PokedexController() {
+        // seed default 10 Pokémon
+        addPokemon(1, "Pikachu",   "Electric", null);
+        addPokemon(2, "Snorlax",   "Normal",   null);
+        addPokemon(3, "Mewtwo",    "Psychic",  null);
+        addPokemon(4, "Charizard", "Fire",    "Flying");
+        addPokemon(5, "Squirtle",  "Water",    null);
+        addPokemon(6, "Eevee",     "Normal",   null);
+        addPokemon(7, "Psyduck",   "Water",    null);
+        addPokemon(8, "Gengar",    "Ghost",   "Poison");
+        addPokemon(9, "Geodude",   "Rock",    "Ground");
+        addPokemon(10, "Onix",      "Rock",    "Ground");
+    }
+
     public void addPokemon(int number, String name, String type1, String type2) {
         pokemons.add(new Pokemon(number, name, type1, type2));
     }
