7f371eb50bf5b9d6a7c5d6dc55799695c228603a
x <xaviertears.dev@gmail.com>
2025-07-24 17:20:57 +0800

    Update TrainersController.java

--- controller/TrainersController.java
+++ controller/TrainersController.java
@@ -4,6 +4,11 @@ import java.util.*;
 public class TrainersController {
     private List<Trainer> trainers = new ArrayList<>();
 
+    public TrainersController() {
+        // seed default trainer
+        addTrainer(1, "Ash Ketchum", 1000000.0);
+    }
+
     public void addTrainer(int id, String name, double money) {
         trainers.add(new Trainer(id, name, money));
     }
