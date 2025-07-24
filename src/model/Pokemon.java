0b28893df0dcc4a7819fec7ad71880909d227c22
x <xaviertears.dev@gmail.com>
2025-07-24 16:35:44 +0800

    Update Pokemon.java

--- src/model/Pokemon.java
+++ src/model/Pokemon.java
@@ -1,1 +1,18 @@
+// Pokemon.java
+public class Pokemon {
+    private int pokedexNumber;
+    private String name;
+    private String type1;
+    private String type2;
 
+    public Pokemon(int pokedexNumber, String name, String type1, String type2) {
+        this.pokedexNumber = pokedexNumber;
+        this.name = name;
+        this.type1 = type1;
+        this.type2 = type2 != null && !type2.isEmpty() ? type2 : null;
+    }
+    public int getPokedexNumber() { return pokedexNumber; }
+    public String getName()       { return name; }
+    public String getType1()      { return type1; }
+    public String getType2()      { return type2; }
+}
