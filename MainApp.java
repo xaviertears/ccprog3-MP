7ed76fddba3ac994ee5965531cd5e9064d3ab47e
x <xaviertears.dev@gmail.com>
2025-07-24 16:28:13 +0800

    Update MainApp.java

--- MainApp.java
+++ MainApp.java
@@ -1,1 +1,146 @@
+// MainApp.java
+import javax.swing.*;
+import java.awt.*;
+
+public class MainApp {
+    private JFrame frame;
+    private JTabbedPane tabbedPane;
+
+    // Controllers
+    private PokedexController pokedexController;
+    private MovesController movesController;
+    private ItemsController itemsController;
+    private TrainersController trainersController;
+
+    // Panels (View)
+    private PokedexPanel pokedexPanel;
+    private MovesPanel movesPanel;
+    private ItemsPanel itemsPanel;
+    private TrainersPanel trainersPanel;
+
+    public MainApp() {
+        // Initialize controllers
+        pokedexController = new PokedexController();
+        movesController   = new MovesController();
+        itemsController   = new ItemsController();
+        trainersController= new TrainersController();
+
+        // Initialize panels and inject controllers
+        pokedexPanel   = new PokedexPanel(pokedexController);
+        movesPanel     = new MovesPanel(movesController);
+        itemsPanel     = new ItemsPanel(itemsController);
+        trainersPanel  = new TrainersPanel(trainersController);
+
+        // Create main window
+        frame = new JFrame("Enhanced Pokédex");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(800, 600);
+
+        // Tabbed pane setup
+        tabbedPane = new JTabbedPane();
+        tabbedPane.addTab("Pokédex", pokedexPanel);
+        tabbedPane.addTab("Moves", movesPanel);
+        tabbedPane.addTab("Items", itemsPanel);
+        tabbedPane.addTab("Trainers", trainersPanel);
+
+        frame.getContentPane().add(tabbedPane, BorderLayout.CENTER);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        // Run UI on Event Dispatch Thread
+        SwingUtilities.invokeLater(MainApp::new);
+    }
+}
+
+// PokedexPanel.java
+import javax.swing.*;
+import java.awt.*;
+
+public class PokedexPanel extends JPanel {
+    private PokedexController controller;
+
+    public PokedexPanel(PokedexController controller) {
+        this.controller = controller;
+        setLayout(new BorderLayout());
+        add(new JLabel("Pokédex Module"), BorderLayout.NORTH);
+        // TODO: Add table and controls to list, add, search Pokémon
+    }
+}
+
+// MovesPanel.java
+import javax.swing.*;
+import java.awt.*;
+
+public class MovesPanel extends JPanel {
+    private MovesController controller;
+
+    public MovesPanel(MovesController controller) {
+        this.controller = controller;
+        setLayout(new BorderLayout());
+        add(new JLabel("Moves Module"), BorderLayout.NORTH);
+        // TODO: Add table and controls to list, add, search moves
+    }
+}
+
+// ItemsPanel.java
+import javax.swing.*;
+import java.awt.*;
+
+public class ItemsPanel extends JPanel {
+    private ItemsController controller;
+
+    public ItemsPanel(ItemsController controller) {
+        this.controller = controller;
+        setLayout(new BorderLayout());
+        add(new JLabel("Items Module"), BorderLayout.NORTH);
+        // TODO: Add table and controls to list, add, search items
+    }
+}
+
+// TrainersPanel.java
+import javax.swing.*;
+import java.awt.*;
+
+public class TrainersPanel extends JPanel {
+    private TrainersController controller;
+
+    public TrainersPanel(TrainersController controller) {
+        this.controller = controller;
+        setLayout(new BorderLayout());
+        add(new JLabel("Trainers Module"), BorderLayout.NORTH);
+        // TODO: Add form and controls to manage trainers
+    }
+}
+
+// PokedexController.java
+public class PokedexController {
+    // TODO: Inject or access your model/service to manage Pokémon
+    public PokedexController() {
+        // Initialize model connections
+    }
+    // Example method
+    public void addPokemon(/* Pokemon data */) {
+        // Delegate to model and update view
+    }
+}
+
+// MovesController.java
+public class MovesController {
+    public MovesController() {}
+    public void addMove(/* Move data */) {}
+}
+
+// ItemsController.java
+public class ItemsController {
+    public ItemsController() {}
+    public void addItem(/* Item data */) {}
+}
+
+// TrainersController.java
+public class TrainersController {
+    public TrainersController() {}
+    public void addTrainer(/* Trainer data */) {}
+}
 
