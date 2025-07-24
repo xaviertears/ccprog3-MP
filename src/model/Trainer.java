2f3757c9ebdc37172ec76f85d786fdc080077446
x <xaviertears.dev@gmail.com>
2025-07-24 16:36:42 +0800

    Update Trainer.java

--- src/model/Trainer.java
+++ src/model/Trainer.java
@@ -1,1 +1,15 @@
+// Trainer.java
+public class Trainer {
+    private int id;
+    private String name;
+    private double money;
 
+    public Trainer(int id, String name, double money) {
+        this.id = id;
+        this.name = name;
+        this.money = money;
+    }
+    public int getId()           { return id; }
+    public String getName()      { return name; }
+    public double getMoney()     { return money; }
+}
