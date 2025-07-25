64ea8bb22a2f60026109e634e0feae63f48a1909
klatido <ken_latido@dlsu.edu.ph>
2025-07-25 13:01:27 +0800

    Update Item.java

--- src/model/Item.java
+++ src/model/Item.java
@@ -1,44 +1,24 @@
-// src/model/Item.java
-package model;
-
-/**
- * An item that can be bought/sold and used on Pokémon.
- */
+// Item.java (model)
 public class Item {
-    private final String name;
-    private final String category;
-    private final String description;
-    private final String effect;
-    private final int buyPrice;
-    private final int sellPrice;
+    private String name;
+    private String category;
+    private String description;
+    private String effect;
+    private int buyPrice;
+    private int sellPrice;
 
-    public Item(String name,
-                String category,
-                String description,
-                String effect,
-                int buyPrice,
-                int sellPrice) {
-        this.name        = name;
-        this.category    = category;
+    public Item(String name, String category, String description, String effect, int buyPrice, int sellPrice) {
+        this.name = name;
+        this.category = category;
         this.description = description;
-        this.effect      = effect;
-        this.buyPrice    = buyPrice;
-        this.sellPrice   = sellPrice;
+        this.effect = effect;
+        this.buyPrice = buyPrice;
+        this.sellPrice = sellPrice;
     }
-
-    // getters...
     public String getName()        { return name; }
     public String getCategory()    { return category; }
     public String getDescription() { return description; }
     public String getEffect()      { return effect; }
     public int getBuyPrice()       { return buyPrice; }
     public int getSellPrice()      { return sellPrice; }
-
-    @Override
-    public String toString() {
-        return String.format(
-            "%-15s [%s] %s – %s  Buy:₽%d Sell:₽%d",
-            name, category, description, effect, buyPrice, sellPrice
-        );
-    }
 }
